e51f3de466eab3aab5b762f57ba4d1948a76f9ff
Michael Smith <msmith@crypticedge.net>
2013-07-19 15:46:36 -0500

    Added directory structure and refactored for classpath changes

---
+++ common/blocks/BlockTantalumOre.java
@@ -0,0 +1,15 @@
+package mod.Geology.common.blocks;
+
+import mod.Geology.common.mod_Geology;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTantalumOre extends Block{
+
+    public BlockTantalumOre(int par1, Material par2Material) {
+        super(par1, par2Material);
+        this.setCreativeTab(mod_Geology.GeologyTab);
+        // TODO Auto-generated constructor stub
+    }
+
+}
